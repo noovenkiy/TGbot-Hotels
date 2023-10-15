2dfbfbb2bcfe0edf5258f1ad0fa2c50a4ceaf1a2
noovenkiy <noovenkiy@yandex.com>
2023-10-16 06:29:03 +0700

    config.py, models.py, database.db added

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_PATH = "database.db"
+BOT_TOKEN = "6417795240:AAEFWX1nOuuH318kpVJIOetp0r1z7hfH3L8"
+
+DEFAULT_COMMANDS = (
+    ("low", "Самая низкая стоимость"),
+    ("high", "Самая высокая стоимость"),
+    ("custom", "Определенная стоимость"),
+    ("history", "История запросов")
+)
